d54f9359888473fb2c51dd81f2bb17be7b299258
Linus Norton <linusnorton@gmail.com>
2020-03-25 17:09:36 +0000

    plugged in phaserg

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,22 @@
+import * as Phaser from "phaser";
+import { MainScene } from "./scene/MainScene";
 
-console.log("Hello world");
+const gameConfig: Phaser.Types.Core.GameConfig = {
+  title: "Sample",
+  parent: "game",
+  backgroundColor: "#000000",
+  type: Phaser.AUTO,
+  scale: {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  scene: [MainScene]
+};
+
+export const game = new Phaser.Game(gameConfig);
