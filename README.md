bcea989c9bbe5d910956f87f64671e7a0c221508
Linus Norton <linusnorton@gmail.com>
2020-03-26 08:53:34 +0000

    Added background

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Evidence Manager - Distributing Justice
 
-Now you too can experience the joys of working in the Department of Justice. Follow the adventures of Justine Corona with her trusty hammer of justice and shield of bureaucracy. 
+Now you too can experience the joys of working in the Department of Justice. 
+
+Follow the adventures of Justine Corona with her trusty Hammer of Justice and Shield of Bureaucracy. 
+
+The Hammer of Justice is arbitrary and the Shield of Bureaucracy is made of paper... will you survive?
+
+## Play now
+
+https://linusnorton.github.io/evidence-manager-game/
 
 ## Run
 
--- src/scene/MainScene.ts
+++ src/scene/MainScene.ts
@@ -5,6 +5,8 @@ export class MainScene extends Scene {
   private player?: Player;
 
   public preload() {
+    this.load.image("backgroundTiles", "image/background/mainlevbuild.png");
+    this.load.tilemapTiledJSON("backgroundMap", "map/main.json");
     this.load.spritesheet(
       playerSprite.name,
       playerSprite.spriteSheet.image,
@@ -13,6 +15,12 @@ export class MainScene extends Scene {
   }
 
   public create() {
+    const map = this.make.tilemap({ key: "backgroundMap" });
+    const tileset = map.addTilesetImage("mainlevbuild", "backgroundTiles");
+    const floor = map.createStaticLayer("Floor", tileset, 0, 0);
+    const walls = map.createStaticLayer("Walls", tileset, 0, 0);
+    walls.setCollisionByProperty({ collides: true });
+
     this.player = new Player(this.physics.add.sprite(100, 450, playerSprite.name, 51));
 
     for (const [name, animation] of Object.entries(playerSprite.animations)) {
